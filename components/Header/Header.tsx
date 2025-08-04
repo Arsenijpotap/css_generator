484eb0b1419d136f0e6b7839ef8024014728f207
Arsenij <arsenij.patap@gmail.com>
2025-08-04 17:17:04 +0300

    updete header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,24 +1,44 @@
 /** @format */
 "use client";
 import useAppStore from "@/stores/appStore";
-import "./style.scss";
+import "./Header.scss";
 import { Tab, Tabs } from "@heroui/tabs";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 function Header() {
+	const pathname = usePathname();
+	useEffect(() => {
+		switch (pathname) {
+			case "/gradient":
+				setMode("gradient");
+				break;
+			case "/neonText":
+				setMode("neon text");
+				break;
+
+			default:
+				break;
+		}
+	});
 	const mode = useAppStore((state) => state.mode);
 	const setMode = useAppStore((state) => state.setMode);
 	return (
 		<header className="header">
 			<h1 className="header__title">Generate your {mode}</h1>
 			<div className="">
-				<Tabs size="lg" variant="underlined" className="header__tabs">
+				<Tabs selectedKey={pathname} size="lg" variant="underlined" className="header__tabs">
 					<Tab
+						key="/gradient"
+						href="/gradient"
 						onClick={() => {
 							setMode("gradient");
 						}}
 						title="Gradient"
 					></Tab>
 					<Tab
+						key="/neonText"
+						href="/neonText"
 						onClick={() => {
 							setMode("neon text");
 						}}
