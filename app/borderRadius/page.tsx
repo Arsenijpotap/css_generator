47cdb876a43444eb90b03769720047d408cb8155
Arsenij <arsenij.patap@gmail.com>
2025-08-09 21:23:14 +0300

    finish border radius sliders

--- app/borderRadius/page.tsx
+++ app/borderRadius/page.tsx
@@ -8,6 +8,7 @@ import { Card } from "@heroui/card";
 import { Select, SelectItem } from "@heroui/select";
 import { Tabs, Tab } from "@heroui/tabs";
 import { Switch } from "@heroui/switch";
+import { Slider } from "@heroui/slider";
 import useGradientStore from "@/stores/gradientStore";
 import off from "@/public/images/off.png";
 import hor from "@/public/images/hor.png";
@@ -54,12 +55,54 @@ export default function BorderRadius() {
 	];
 	const unit = useBorderRadiusStore((state) => state.unit);
 	const setUnit = useBorderRadiusStore((state) => state.setUnit);
-
+	const radiusValues = useBorderRadiusStore((state) => state.radiusValues);
+	const setValue = useBorderRadiusStore((state) => state.setValue);
+	const setSplitValues = useBorderRadiusStore((state) => state.setSplitValues);
+	const isSimmetryActive = useBorderRadiusStore((state) => state.isSimmetryActive);
+	const toggleSimmetryActive = useBorderRadiusStore((state) => state.toggleSimmetryActive);
+	const linkType = useBorderRadiusStore((state) => state.linkType);
+	const setLinkType = useBorderRadiusStore((state) => state.setLinkType);
+	const updateValues = useBorderRadiusStore((state) => state.updateValues);
+	let borderRadiusCss = +!isSimmetryActive
+		? "" +
+			radiusValues["top-left"].first +
+			unit +
+			" " +
+			radiusValues["top-right"].first +
+			unit +
+			" " +
+			radiusValues["bottom-right"].first +
+			unit +
+			" " +
+			radiusValues["bottom-left"].first +
+			unit +
+			" " +
+			"/  " +
+			radiusValues["top-left"].second +
+			unit +
+			" " +
+			radiusValues["top-right"].second +
+			unit +
+			" " +
+			radiusValues["bottom-right"].second +
+			unit +
+			" " +
+			radiusValues["bottom-left"].second +
+			unit
+		: "" + radiusValues["top-left"].first + unit + " " + radiusValues["top-right"].first + unit + " " + radiusValues["bottom-right"].first + unit + " " + radiusValues["bottom-left"].first + unit + " ";
+	console.log(borderRadiusCss);
+	let index = 1;
+	linkTypes.forEach((value, ind) => {
+		console.log(value.name, linkType);
+		if (value.name.toLowerCase() == linkType) {
+			index = ind + 1;
+		}
+	});
 	return (
 		<div className="conteiner">
 			<div className="borderRadius main">
 				<Card className="box">
-					<div className="borderRadius__example"></div>
+					<div className="borderRadius__example" style={{ borderRadius: borderRadiusCss }}></div>
 				</Card>
 				<Card className="settings">
 					<div className="borderRadius__wrapper">
@@ -75,7 +118,7 @@ export default function BorderRadius() {
 								// size=""
 								// label="Link type"
 								labelPlacement="outside"
-								defaultSelectedKeys={"2"}
+								defaultSelectedKeys={index.toString()}
 								placeholder="Select a link type"
 								renderValue={(items) => {
 									return items.map((item) => (
@@ -89,7 +132,14 @@ export default function BorderRadius() {
 								}}
 							>
 								{(type) => (
-									<SelectItem key={type.id} textValue={type.name}>
+									<SelectItem
+										onClick={() => {
+											setLinkType((type.name.toLowerCase() as "off" | "all" | "horizontal" | "vertical" | "diagonal") || "off");
+											updateValues();
+										}}
+										key={type.id}
+										textValue={type.name}
+									>
 										<div className="flex gap-2 items-center">
 											<Image alt={type.name} className="borderRadius__avatar shrink-0" src={type.avatar} />
 											<div className="flex flex-col">
@@ -111,16 +161,129 @@ export default function BorderRadius() {
 					<div className="borderRadius__wrapper">
 						<p className="borderRadius__text">Symmetry</p>
 
-						<Switch defaultSelected size="sm"></Switch>
+						<Switch
+							onChange={(e) => {
+								toggleSimmetryActive();
+								setTimeout(() => {
+									directions.forEach((arr) => {
+										let name = (arr[0] + "-" + arr[1]) as "top-right" | "top-left" | "bottom-left" | "bottom-right";
+										setValue(name, radiusValues[name].first);
+									});
+									console.log(isSimmetryActive);
+								}, 100);
+							}}
+							defaultSelected
+							size="sm"
+							isSelected={isSimmetryActive}
+						></Switch>
 					</div>
 					<div className="borderRadius__line"></div>
 					{directions.map((arr, index) => {
 						let rotate = -90 + index * 90;
+						let name = (arr[0] + "-" + arr[1]) as "top-right" | "top-left" | "bottom-left" | "bottom-right";
+						console.log(name);
 						return (
-							<div className="borderRadius__radiusBlock">
-								<Image className="borderRadius__avatar" style={{ rotate: rotate + "deg" }} alt={arr[0] + "-" + arr[1] + " direction"} src={border}></Image>
-								<p className="borderRadius__text">{arr[0] + "-" + arr[1]}</p>
-								<div className="borderRadius__line"></div>
+							<div key={name}>
+								<div className="borderRadius__radiusBlock">
+									<Image className="borderRadius__avatar" style={{ rotate: rotate + "deg" }} alt={arr[0] + "-" + arr[1] + " direction"} src={border}></Image>
+									<div className="borderRadius__box">
+										{/* <p className="borderRadius__text">{name}</p> */}
+										<div className="borderRadius__slidersWrapper">
+											{isSimmetryActive ? (
+												<div style={{ width: isSimmetryActive ? "330px" : "" }} className="borderRadius__sliderWrapper">
+													<Slider
+														value={radiusValues[name].first}
+														onChange={(e) => {
+															setValue(name, typeof e == "number" ? e : e[0]);
+														}}
+														aria-label="Radius value"
+														className="max-w-md borderRadius__firstValueSlider"
+														maxValue={100}
+														minValue={0}
+														size="sm"
+														step={1}
+													/>
+													<input
+														max={100}
+														min={0}
+														aria-label="Radius value"
+														className="borderRadius__firstValue"
+														type="number"
+														value={radiusValues[name].first}
+														onChange={(e) => {
+															const v = parseFloat(e.target.value);
+															if (v <= 100) setValue(name, v);
+															if (v > 100) setValue(name, 100);
+															if (v < 0) setValue(name, 0);
+															if (Number.isNaN(v)) setValue(name, 0);
+														}}
+													/>
+												</div>
+											) : (
+												<div style={{ width: isSimmetryActive ? "330px" : "" }} className="borderRadius__sliderWrapper">
+													<Slider
+														value={radiusValues[name].first}
+														onChange={(e) => {
+															setSplitValues(name, typeof e == "number" ? e : e[0], radiusValues[name].second);
+														}}
+														aria-label="Radius value"
+														className="max-w-md borderRadius__firstValueSlider"
+														maxValue={100}
+														minValue={0}
+														size="sm"
+														step={1}
+													/>
+													<input
+														max={100}
+														min={0}
+														aria-label="Radius value"
+														className="borderRadius__firstValue"
+														type="number"
+														value={radiusValues[name].first}
+														onChange={(e) => {
+															const v = parseFloat(e.target.value);
+															if (v <= 100) setSplitValues(name, v, radiusValues[name].second);
+															if (v > 100) setSplitValues(name, 100, radiusValues[name].second);
+															if (v < 0) setSplitValues(name, 0, radiusValues[name].second);
+															if (Number.isNaN(v)) setSplitValues(name, 0, radiusValues[name].second);
+														}}
+													/>
+												</div>
+											)}
+											<div style={{}} className="borderRadius__sliderWrapper">
+												<Slider
+													value={radiusValues[name].second}
+													onChange={(e) => {
+														setSplitValues(name, radiusValues[name].first, typeof e == "number" ? e : e[0]);
+													}}
+													aria-label="Radius value"
+													className="max-w-md borderRadius__firstValueSlider"
+													maxValue={100}
+													minValue={0}
+													size="sm"
+													step={1}
+												/>
+												<input
+													max={100}
+													min={0}
+													aria-label="Radius value"
+													className="borderRadius__firstValue"
+													type="number"
+													value={radiusValues[name].second}
+													onChange={(e) => {
+														const v = parseFloat(e.target.value);
+														if (v <= 100) setSplitValues(name, radiusValues[name].first, v);
+														if (v > 100) setSplitValues(name, radiusValues[name].first, 100);
+														if (v < 0) setSplitValues(name, radiusValues[name].first, 0);
+														if (Number.isNaN(v)) setSplitValues(name, radiusValues[name].first, 0);
+													}}
+												/>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								{index < 3 ? <div className="borderRadius__line"></div> : <></>}
 							</div>
 						);
 					})}
--- stores/borderRadiusStore.ts
+++ stores/borderRadiusStore.ts
@@ -6,10 +6,150 @@ import { create } from "zustand";
 interface BorderRadiusStore {
 	unit: "%" | "px";
 	setUnit: (val: "%" | "px") => void;
+
+	linkType: "off" | "all" | "horizontal" | "vertical" | "diagonal";
+	setLinkType: (type: "off" | "all" | "horizontal" | "vertical" | "diagonal") => void;
+
+	isSimmetryActive: boolean;
+	toggleSimmetryActive: () => void;
+
+	radiusValues: {
+		"top-left": {
+			first: number;
+			second: number;
+		};
+		"top-right": {
+			first: number;
+			second: number;
+		};
+		"bottom-left": {
+			first: number;
+			second: number;
+		};
+		"bottom-right": {
+			first: number;
+			second: number;
+		};
+	};
+	setValue: (pos: "top-right" | "top-left" | "bottom-left" | "bottom-right", value: number) => void;
+	setSplitValues: (pos: "top-right" | "top-left" | "bottom-left" | "bottom-right", firstValue: number, secondValue: number) => void;
+	updateValues: () => void;
 }
 
+const linkScheme = {
+	off: [],
+	all: [["top-right", "top-left", "bottom-left", "bottom-right"]],
+	horizontal: [
+		["top-right", "top-left"],
+		["bottom-left", "bottom-right"],
+	],
+	diagonal: [],
+	vertical: [],
+};
+
 const useBorderRadiusStore = create<BorderRadiusStore>()((set) => ({
+	linkType: "off",
+	setLinkType: (type) => set((state) => ({ linkType: type })),
+
+	isSimmetryActive: true,
+	toggleSimmetryActive: () => set((state) => ({ isSimmetryActive: !state.isSimmetryActive })),
+
 	unit: "px",
 	setUnit: (value) => set((state) => ({ unit: value })),
+
+	setValue: (pos, val) =>
+		set((state) => {
+			let finalObj = {
+				[pos]: {
+					first: val,
+					second: val,
+				},
+			};
+			linkScheme[state.linkType].forEach((arr) => {
+				if (arr.includes(pos)) {
+					arr.forEach((name) => {
+						finalObj[name] = {
+							first: val,
+							second: val,
+						};
+					});
+				}
+			});
+			return {
+				radiusValues: {
+					...state.radiusValues,
+					...finalObj,
+				},
+			};
+		}),
+	setSplitValues: (pos, val1, val2) =>
+		set((state) => {
+			let finalObj = {
+				[pos]: {
+					first: val1,
+					second: val2,
+				},
+			};
+			linkScheme[state.linkType].forEach((arr) => {
+				if (arr.includes(pos)) {
+					arr.forEach((name) => {
+						finalObj[name] = {
+							first: val1,
+							second: val2,
+						};
+					});
+				}
+			});
+			return {
+				radiusValues: {
+					...state.radiusValues,
+					...finalObj,
+				},
+			};
+		}),
+	radiusValues: {
+		"top-left": {
+			first: 50,
+			second: 50,
+		},
+		"top-right": {
+			first: 50,
+			second: 50,
+		},
+		"bottom-left": {
+			first: 50,
+			second: 50,
+		},
+		"bottom-right": {
+			first: 50,
+			second: 50,
+		},
+	},
+	updateValues: () =>
+		set((state) => {
+			// Создаем копию текущих значений
+			const newRadiusValues = { ...state.radiusValues };
+
+			// Обновляем значения согласно схеме связей
+			linkScheme.all[0].forEach((direction) => {
+				const val1 = state.radiusValues[direction as "top-right" | "top-left" | "bottom-left" | "bottom-right"].first;
+				const val2 = state.radiusValues[direction as "top-right" | "top-left" | "bottom-left" | "bottom-right"].second;
+
+				// Обновляем текущее направление
+				newRadiusValues[direction as "top-right" | "top-left" | "bottom-left" | "bottom-right"] = { first: val1, second: val2 };
+
+				// Обновляем связанные направления
+				linkScheme[state.linkType].forEach((arr) => {
+					if (arr.includes(direction)) {
+						arr.forEach((name) => {
+							newRadiusValues[name as "top-right" | "top-left" | "bottom-left" | "bottom-right"] = { first: val1, second: val2 };
+						});
+					}
+				});
+			});
+
+			// Возвращаем обновленное состояние
+			return { radiusValues: newRadiusValues };
+		}),
 }));
 export default useBorderRadiusStore;
