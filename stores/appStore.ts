6632d65502196feb995b05ef92628f3e7b1c20f6
Arsenij <arsenij.patap@gmail.com>
2025-08-22 14:10:51 +0300

    add saving theme state

--- stores/appStore.ts
+++ stores/appStore.ts
@@ -1,30 +1,34 @@
 /** @format */
 
 import { create } from "zustand";
-
+import { persist, createJSONStorage } from "zustand/middleware";
 interface AppStore {
 	mode: "gradient" | "border radius" | "neon text";
 	setMode: (val: "gradient" | "border radius" | "neon text") => void;
-
-	copied: boolean;
-	setCopied: (val: boolean) => void;
 
 	theme: "light" | "dark" | "system";
 	setTheme: (val: "light" | "dark" | "system") => void;
 }
 
-const useAppStore = create<AppStore>()((set) => ({
-	mode: "gradient",
-	setMode: (value) =>
-		set((state) => {
-			console.log(state.mode, value);
-			return { mode: value };
+const useAppStore = create<AppStore>()(
+	persist(
+		(set) => ({
+			mode: "gradient",
+			setMode: (value) =>
+				set((state) => {
+					console.log(state.mode, value);
+					return { mode: value };
+				}),
+
+			theme: "system",
+			setTheme: (value) => set({ theme: value }),
 		}),
+		{
+			name: "app-storage",
+			storage: createJSONStorage(() => localStorage),
 
-	copied: false,
-	setCopied: (value) => set((state) => ({ copied: value })),
-
-	theme: "system",
-	setTheme: (value) => set((state) => ({ theme: value })),
-}));
+			partialize: (state) => ({ theme: state.theme }),
+		}
+	)
+);
 export default useAppStore;
