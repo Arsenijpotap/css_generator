9c5e0e7a011efcaa441310eadc3532a27a722ccd
Arsenij <arsenij.patap@gmail.com>
2025-08-22 20:21:12 +0300

    add config info

--- config/site.ts
+++ config/site.ts
@@ -1,69 +1,47 @@
+/** @format */
+
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
-  navItems: [
-    {
-      label: "Home",
-      href: "/",
-    },
-    {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
-    },
-  ],
-  navMenuItems: [
-    {
-      label: "Profile",
-      href: "/profile",
-    },
-    {
-      label: "Dashboard",
-      href: "/dashboard",
-    },
-    {
-      label: "Projects",
-      href: "/projects",
-    },
-    {
-      label: "Team",
-      href: "/team",
-    },
-    {
-      label: "Calendar",
-      href: "/calendar",
-    },
-    {
-      label: "Settings",
-      href: "/settings",
-    },
-    {
-      label: "Help & Feedback",
-      href: "/help-feedback",
-    },
-    {
-      label: "Logout",
-      href: "/logout",
-    },
-  ],
-  links: {
-    github: "https://github.com/heroui-inc/heroui",
-    twitter: "https://twitter.com/hero_ui",
-    docs: "https://heroui.com",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
-  },
+	name: "CSS Generator",
+	description: "Generate beautiful CSS code for gradients, neon text effects, and border radius with easy-to-use visual tools.",
+	navItems: [
+		{
+			label: "Home",
+			href: "/",
+		},
+		{
+			label: "Gradient",
+			href: "/gradient",
+		},
+		{
+			label: "Neon Text",
+			href: "/neonText",
+		},
+		{
+			label: "Border Radius",
+			href: "/borderRadius",
+		},
+	],
+	navMenuItems: [
+		{
+			label: "Gradient Generator",
+			href: "/gradient",
+		},
+		{
+			label: "Neon Text Generator",
+			href: "/neonText",
+		},
+		{
+			label: "Border Radius Generator",
+			href: "/borderRadius",
+		},
+		{
+			label: "Home",
+			href: "/",
+		},
+	],
+	links: {
+		github: "https://github.com/Arsenijpotap/css_generator",
+	},
 };
